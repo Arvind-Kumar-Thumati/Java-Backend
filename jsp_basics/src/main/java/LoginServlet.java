c217a2d11476b726c392f370755f26c907473e31
Arvind Kumar <trarvindkumar01@gmail.com>
2022-02-03 20:41:52 +0530

    Update on servlet and index

--- jsp_basics/src/main/java/LoginServlet.java
+++ jsp_basics/src/main/java/LoginServlet.java
@@ -7,9 +7,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class LoginServlet extends HttpServlet {
-    @Override
+    @Override    
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         PrintWriter out = res.getWriter();
-        out.println("Inside loginServlet");
+        out.println("Inside loginServlet. Welcome ! "+req.getParameter("uName"));
     }
 }
--- jsp_basics/src/main/webapp/index.jsp
+++ jsp_basics/src/main/webapp/index.jsp
@@ -1,7 +1,7 @@
 <html>
 <body>
 <h2>Hello World!</h2>
-<form action="login" method="post">
+<form method = "get" action="login">
     Enter your name : <input type="text" name ="uName"><br>
     <input type="submit" value = "submit">
 </form>
